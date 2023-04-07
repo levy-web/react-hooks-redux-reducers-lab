f73d4d9bfd71e9a5866b5e33ad2ba7b36c488549
levy <lnjiru43@gmail.com>
2023-04-07 21:48:07 +0300

    completed assignment

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,15 @@
-export function manageFriends(state, action) {
+const initialState = { friends: [] };
+
+export function manageFriends(state = initialState, action) {
   // your code here
+  switch (action.type) {
+    case "friends/add":
+      return { friends: [...state.friends, action.payload]  };
+    case "friends/remove":
+
+      return { friends: state.friends.splice(action.payload)};  
+    default:
+      return state;
+  }
+
 }
